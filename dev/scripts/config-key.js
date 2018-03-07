75dab532bb4b8e5d4a0da6a0b17db2484cca0769
Cece Fan <cecilian.fan123@gmail.com>
2018-03-07 10:38:03 -0500

    made axios call https

--- dev/scripts/config-key.js
+++ dev/scripts/config-key.js
@@ -6,7 +6,7 @@
 // }
 
 export default {
-    apiURLS: 'http://lcboapi.com/stores',
-    apiURLP: 'http://lcboapi.com/products',
+    apiURLS: 'https://lcboapi.com/stores',
+    apiURLP: 'https://lcboapi.com/products',
     apiKey: 'MDo3MzM2MzkyMC0xY2ExLTExZTgtYTBkNS05YmRkMmU5N2NiMDA6TmRoZEFEdVpNV0s5Rm05S3VSRVlyVUZ3U1dYVjg3T21hYVRB'
 }
